ef49865e9237c1158204b78632f926ad543d299d
L. Pesola <lpesola@cs.helsinki.fi>
2016-10-18 18:09:55 +0300

    fix indents

--- ex2/mouse.py
+++ ex2/mouse.py
@@ -15,16 +15,16 @@ def die(s, addr):
     # attack on the mouse "should take 6 seconds" so we wait (though since "the cat will wait 8 seconds for the ouch message"
     # it is a bit unclear whether this 6 seconds is already included in the cat's 8 second wait)
     time.sleep(6)
-	f = s.makefile("rw")
-	msg = f.readline()
-	if msg == "MEOW":
-		f.write("ouch")
-		f.flush()
-		global alive
-		global lock
-		lock.acquire()
-		alive = False
-		lock.release()
+    f = s.makefile("rw")
+    msg = f.readline()
+    if msg == "MEOW":
+        f.write("ouch")
+        f.flush()
+        global alive
+        global lock
+        lock.acquire()
+        alive = False
+        lock.release()
 
 # listen to the port defined in port_number for connections
 # assumption: only the one cat program doing the attack will connect so after we get the
@@ -36,20 +36,19 @@ portno = int(f.readline().rstrip())
 f.close()
 
 try:
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.bind((socket.gethostname(), portno))
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((socket.gethostname(), portno))
 except Exception as e:
-	print (str(e))
+    print (str(e))
 
 while alive:
-	# there should be only one connection incoming for the cat
-	s.listen(2)
-	(cs, addr) = s.accept()
-	mouset = threading.Thread(target=die, args=(cs, addr))
-	mouset.start()
-	mouset.join()
-	s.close()
-
+    # there should be only one connection incoming for the cat
+    s.listen(2)
+    (cs, addr) = s.accept()
+    mouset = threading.Thread(target=die, args=(cs, addr))
+    mouset.start()
+    mouset.join()
+    s.close()
 
 
 
